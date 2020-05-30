b903780ea357e553dbb2f7c819c40a7e218bf343
Emma <gigitxl@yahoo.ca>
2020-05-30 16:00:24 -0700

    add price operations

---
+++ PSO2ShopAid/PriceOp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSO2ShopAid
+{
+    public static class PriceOp
+    {
+        public static Price Subtract(this Price a, Price b)
+        {
+            return new Price(a.RawPrice - b.RawPrice);
+        }
+
+        public static Price Add(this Price a, Price b)
+        {
+            return new Price(a.RawPrice + b.RawPrice);
+        }
+    }
+}
