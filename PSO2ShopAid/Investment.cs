d22cbcb7c7617553fe0d048e0c3c026336bb6732
Emma <gigitxl@yahoo.ca>
2020-05-30 16:09:00 -0700

    add investment model

--- PSO2ShopAid/Investment.cs
+++ PSO2ShopAid/Investment.cs
@@ -10,5 +10,52 @@ namespace PSO2ShopAid
     public class Investment
     {
         public DateTime PurchaseDate { get; set; }
+        public Price PurchasePrice { get; set; }
+        public DateTime SellDate { get; set; }
+        public Price SellPrice { get; set; }
+        public bool IsSold { get { return SellPrice != null; } }
+
+
+        public Investment(DateTime date, Price price)
+        {
+            PurchaseDate = date;
+            PurchasePrice = price;
+        }
+
+        public void Sell(DateTime date, Price price)
+        {
+            SellDate = date;
+            SellPrice = price;
+        }
+
+        public Price NetGain()
+        {
+            if (!IsSold)
+            {
+                return new Price(0);
+            }
+
+            return SellPrice.Subtract(PurchasePrice);
+        }
+
+        public float PercentGain()
+        {
+            if (!IsSold)
+            {
+                return 0;
+            }
+
+            return SellPrice.PercentChange(PurchasePrice);
+        } 
+
+        public TimeSpan FlipTime()
+        {
+            if (SellDate == null)
+            {
+                return new TimeSpan(0);
+            }
+
+            return SellDate.Subtract(PurchaseDate);
+        }
     }
 }
